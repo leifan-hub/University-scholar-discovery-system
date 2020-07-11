3c0db808c2902f15d72e2b27f12e007d754639f2
lljshuai <61152371+lljshuai@users.noreply.github.com>
2020-07-11 20:03:20 +0800

    第一版

--- SSM/src/com/ssm/mapper/ExpertMapper.java
+++ SSM/src/com/ssm/mapper/ExpertMapper.java
@@ -1,12 +1,21 @@
 package com.ssm.mapper;
 
 import java.util.List;
+
+import org.springframework.web.servlet.ModelAndView;
+
 import com.ssm.domain.Expert;
 
 
 public interface ExpertMapper {
 	public List<Expert> getExpertList();
 	public List<Expert> getExpertByName(String name);
+	public List<Expert> getExpertBySchool(String school);
+	public List<Expert> getExpertByMajor(String major);
+	public List<Expert> getExpertBySubject(String subject);
+	public List<Expert> getExpertByPaper(String paper);
+	public List<Expert> getExpertByResearch(String research_direction);
+	public List<Expert> getExpertByIntroduction(String introduction);
 	public Expert getExpertById(int id);
 	public int addExpert(Expert expert);
 	public int delete(int id);
