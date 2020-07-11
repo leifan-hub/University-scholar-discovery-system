efe89268c9bca728ed4f987edca29d422b74ef9b
lljshuai <61152371+lljshuai@users.noreply.github.com>
2020-07-11 20:04:11 +0800

    第一版

--- SSM/src/com/ssm/service/impl/ExpertServiceImpl.java
+++ SSM/src/com/ssm/service/impl/ExpertServiceImpl.java
@@ -3,6 +3,7 @@ package com.ssm.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.ssm.domain.Expert;
 import com.ssm.mapper.ExpertMapper;
@@ -15,18 +16,32 @@ public class ExpertServiceImpl implements ExpertService {
 
 	
 	@Override
-	public List<Expert> getExpertList() {
-		
+	public List<Expert> getExpertList() {		
 		return expertMapper.getExpertList();
 	}
-	public List<Expert> getExpertByName(String name){
-		
-	return expertMapper.getExpertByName(name);
+	public List<Expert> getExpertByName(String name){		
+		return expertMapper.getExpertByName(name);
 	}
-	
+	public List<Expert> getExpertBySchool(String school){		
+		return expertMapper.getExpertBySchool(school);
+	}
+	public List<Expert> getExpertByMajor(String major){		
+		return expertMapper.getExpertByMajor(major);
+	}
+	public List<Expert> getExpertBySubject(String subject){		
+		return expertMapper.getExpertBySubject(subject);
+	}
+	public List<Expert> getExpertByResearch(String research_direction){		
+		return expertMapper.getExpertByResearch(research_direction);
+	}
+	public List<Expert> getExpertByPaper(String paper){		
+		return expertMapper.getExpertByPaper(paper);
+	}	
+	public List<Expert> getExpertByIntroduction(String introduction){		
+		return expertMapper.getExpertByIntroduction(introduction);
+	}
 	public Expert getExpertById(int id) {
-		return expertMapper.getExpertById(id);
-		
+		return expertMapper.getExpertById(id);		
 	}
 	public int addExpert(Expert expert) {
 		
@@ -41,6 +56,7 @@ public class ExpertServiceImpl implements ExpertService {
 	public int delete(int id) {
 		return expertMapper.delete(id);
 	}
+
 	
 	
 }
