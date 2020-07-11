752059c267345393615f42a0b6241033503b7306
TJW-0 <61374195+TJW-0@users.noreply.github.com>
2020-07-11 17:22:58 +0800

    Update ExpertMapper.java
    同上，增加其他的查询方法。

--- SSM/src/com/ssm/mapper/ExpertMapper.java
+++ SSM/src/com/ssm/mapper/ExpertMapper.java
@@ -7,6 +7,11 @@ import com.ssm.domain.Expert;
 public interface ExpertMapper {
 	public List<Expert> getExpertList();
 	public List<Expert> getExpertByName(String name);
+	public List<Expert> getExpertBySchool(String school);
+	public List<Expert> getExpertByMajor(String major);
+	public List<Expert> getExpertBySubject(String subject);
+	public List<Expert> getExpertByPaper(String paper);
+	public List<Expert> getExpertByResearchDirection(String research_direction);
 	public Expert getExpertById(int id);
 	public int addExpert(Expert expert);
 	public int delete(int id);
